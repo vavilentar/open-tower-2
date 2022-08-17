c5e11b7e7226e50aab72aae0f46d9487ac270e73
Roman Pchelkin <epos.91@mail.ru>
2022-08-17 15:36:41 +0300

    fix

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 const data = document.querySelectorAll('.text-input');
 const btn = document.querySelector('.btn-sb');
+const form = document.querySelector('.wrapper');
+const modal = document.querySelector('.success-block');
+const labels = document.querySelectorAll('.form-control label');
 
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
+	e.preventDefault();	
 	console.log(getData())
 })
 
 function getData() {
 
-	let dataObj = {
-		name: data[0].value,
-		phone: data[1].value,
-		mail: data[2].value,
+	if (data[0].value != '' && data[1] != '' && data[2] != '' && document.querySelector('.checkbox').checked == true) {
+		let dataObj = {
+			name: data[0].value,
+			phone: data[1].value,
+			mail: data[2].value,
+		}
+
+		form.classList.remove('wrapper-show');
+		modal.classList.add('success-show');
+
+		return JSON.stringify(dataObj);
+	} else {
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].value == '') {
+				data[i].style = 'border-bottom: 2px solid red';
+			}
+		}
 	}
-	return JSON.stringify(dataObj);
 }
 
-// function resetForms () {
-// 	for (let i = 0; i < data.length; i++) {
-// 		data[i].value = '';
-// 	}
-// 	document.querySelector('.checkbox').checked = false;
-// }
-
-const labels = document.querySelectorAll('.form-control label');
 
 labels.forEach(label => {
 	label.innerHTML = label.innerText
-	.split('')
-	.map((letter, index) => `<span style="color: white; transition-delay: ${index * 50}ms">${letter}</span>`)
-	.join('');
+		.split('')
+		.map((letter, index) => `<span style="color: white; transition-delay: ${index * 50}ms">${letter}</span>`)
+		.join('');
 });
